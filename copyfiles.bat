904b763d9f53fdef2404dc077bc179d1ed3aaf69
Justin Young <justin_dean_young@yahoo.com>
2024-08-05 12:23:28 -0400

    fix sw.js copy

--- copyfiles.bat
+++ copyfiles.bat
@@ -4,3 +4,4 @@ cp manifest.json ./public/assets/manifest.json
 cp ./src/assets/cog.png ./public/assets/cog.png
 cp ./src/style.css ./public/assets/style.cc
 cp offline.html ./public/assets/offline.html
+cp ./src/sw.js ./public/assets/sw.js

c3a869fd2e25c6227ee2664adf8a721391929f47
Justin Young <justin_dean_young@yahoo.com>
2024-08-05 12:29:41 -0400

    moved offline.html to public

--- copyfiles.bat
+++ copyfiles.bat
@@ -3,5 +3,5 @@ cp iconbig.png ./public/assets/iconbig.png
 cp manifest.json ./public/assets/manifest.json
 cp ./src/assets/cog.png ./public/assets/cog.png
 cp ./src/style.css ./public/assets/style.cc
-cp offline.html ./public/assets/offline.html
+cp offline.html ./public/offline.html
 cp ./src/sw.js ./public/sw.js

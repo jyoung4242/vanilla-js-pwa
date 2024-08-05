68210bedacc3963ac80f8c0d944d881b992927c1
Justin Young <justin_dean_young@yahoo.com>
2024-08-05 12:13:43 -0400

    updated all static assets

---
+++ copyfiles.bat
@@ -0,0 +1,6 @@
+cp icon.png ./public/assets/icon.png
+cp iconbig.png ./public/assets/iconbig.png
+cp manifest.json ./public/assets/manifest.json
+cp ./src/assets/cog.png ./public/assets/cog.png
+cp ./src/style.css ./public/assets/style.cc
+cp offline.html ./public/assets/offline.html
